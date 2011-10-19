e89b8b1ab7ee5d9f9233ec34ae7e99082e7d81cf
Mark Harrison <mark@mivok.net>
2011-10-19 15:07:06 -0400

    Use the final paths on a live system for cookbooks/roles

--- solo.rb
+++ solo.rb
@@ -1,4 +1,4 @@
 # chef-solo configuration
-file_cache_path "/var/chef-solo"
-cookbook_path File.join(File.dirname(__FILE__), "cookbooks")
-role_path File.join(File.dirname(__FILE__), "roles")
+file_cache_path "/var/chef-solo/cache"
+cookbook_path "/var/chef-solo/config/cookbooks"
+role_path "/var/chef-solo/config/roles"
