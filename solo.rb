d6acefffd1fc71480d6b61ebdc4f5d99a6df4799
Mark Harrison <mark@mivok.net>
2011-11-04 10:14:22 -0400

    Update chef config to include common cookbooks

--- solo.rb
+++ solo.rb
@@ -1,4 +1,5 @@
 # chef-solo configuration
 file_cache_path "/var/chef-solo/cache"
-cookbook_path "/var/chef-solo/config/cookbooks"
+cookbook_path ["/var/chef-solo/common/cookbooks",
+    "/var/chef-solo/config/cookbooks"]
 role_path "/var/chef-solo/config/roles"
