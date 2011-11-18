41e1f9d5d0226c1f5d9d698ed0de70a4b37b56f3
Mark Harrison <mark@mivok.net>
2011-11-18 16:37:41 -0500

    Fix insecure ssh setting
    Annoyingly, there is no setting which says 'trust on first connect', but
    refuse if the ssh key changes. StrictHostKeyChecking no does most of this, but
    if the ssh key changes, it still allows key based auth through (it just
    disables password auth).

--- resources/git-ssh-wrapper.sh
+++ resources/git-ssh-wrapper.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
+##############################################################################
 # Git ssh wrapper - manually specify the key to use
-ssh -i /root/.ssh/id_rsa_chef -o "StrictHostKeyChecking no" "$@"
+##############################################################################
+# This requires that the key be present in the hosts file already. Use:
+#       ssh-keyscan [hostname] >> /root/.ssh/known_hosts
+##############################################################################
+ssh -i /root/.ssh/id_rsa_chef "$@"
