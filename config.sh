c62a5d426c26a5d34aeb7488560e6ece5b9bda69
Mark Harrison <mark@mivok.net>
2011-11-30 16:00:43 -0500

    We're not running on solaris here - comment out the config specific to that

--- config.sh
+++ config.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # Fix various paths to get chef-solo working
-export PATH=$PATH:/opt/omni/lib/ruby/gems/1.8/gems/chef-0.10.4/bin
-export GEM_PATH=/opt/omni/lib/ruby/gems/1.8
-export GEM_HOME=/opt/omni/lib/ruby/gems/1.8
+#export PATH=$PATH:/opt/omni/lib/ruby/gems/1.8/gems/chef-0.10.4/bin
+#export GEM_PATH=/opt/omni/lib/ruby/gems/1.8
+#export GEM_HOME=/opt/omni/lib/ruby/gems/1.8
 
 # Set path so git works
-export PATH=$PATH:/opt/omni/bin
+#export PATH=$PATH:/opt/omni/bin
 
 INTERVAL=1800
 SPLAY=120 # Random interval to inititally sleep to stagger chef runs
