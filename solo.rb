39178c5967204d8202428115533cf38d0f55d185
Mark Harrison <mark@mivok.net>
2011-12-02 15:08:15 -0500

    Add data bag path for the chef-solo-search feature

--- solo.rb
+++ solo.rb
@@ -3,3 +3,4 @@ file_cache_path "/var/chef-solo/cache"
 cookbook_path ["/var/chef-solo/common/cookbooks",
     "/var/chef-solo/config/cookbooks"]
 role_path "/var/chef-solo/config/roles"
+data_bag_path "/var/chef-solo/config/data_bags"
