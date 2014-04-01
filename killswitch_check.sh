aec31a666e5748d177797acef8664ff4c5d0a2be
mmurphy <mmurphy@omniti.com>
2014-04-01 18:01:57 -0400

    tid25320 fix syntax error

--- killswitch_check.sh
+++ killswitch_check.sh
@@ -3,10 +3,25 @@
 # This script will check to see if a killswitch is in place
 # and email the given email address if so.
 
-SUBJECT="Chef disabled"
 EMAIL=$1
-MESSAGE="This is an alert that someone has created a killswitch and chef will be disabled until it is manually removed."
+HOST=`hostname`
+KSFILE="/var/chef-solo/scripts/killswitch"
+if [ -f $KSFILE ]; then
+    KSINFO=`ls -l /var/chef-solo/scripts/killswitch`
+    TMPFILE=`mktemp /tmp/ksXXXX`
 
-if [ -f /var/chef-solo/scripts/killswitch]; then
-    /bin/mail -s "$SUBJECT" "$EMAIL" < $MESSAGE
+cat > $TMPFILE <<EOF
+Subject: Chef disabled
+
+This is an alert that someone has created a killswitch and chef will be disabled until it is manually removed.
+
+Host: $HOST
+$KSINFO
+EOF
+
+    if [ -x /usr/lib/sendmail ]; then
+       /usr/lib/sendmail -oi $EMAIL < $TMPFILE
+    fi
+
+    rm $TMPFILE
 fi
